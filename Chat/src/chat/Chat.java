162b16c8462606710325dd31769383e2db4b027b
a1320548 <a1320548@L2-08.HTO.IFSP.EDU.BR>
2015-02-12 09:50:11 -0200

    Comentário

---
+++ Chat/src/chat/Chat.java
@@ -0,0 +1,20 @@
+/*
+ * Só Lamento
+ */
+
+package chat;
+
+/**
+ *
+ * @author a1320548
+ */
+public class Chat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
