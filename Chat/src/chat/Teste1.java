c5fdec83d0f452b1f8ab280ba7d363f53bdf477c
a1320548 <a1320548@L2-08.HTO.IFSP.EDU.BR>
2015-02-12 12:06:56 -0200

    Criação da classe do 1º caso de teste

---
+++ Chat/src/chat/Teste1.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package chat;
+
+import java.sql.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author a1320548
+ */
+public class Teste1 {
+
+    /**Caso de teste 1**/
+    /*Criar um objeto do tipo mensagem, escrevê-la no banco de dados usando o metodo
+    MensagemDAO.InserirNovaMensagem(). Utilizar o método MensagemDAO.ObterMensagens
+    para obter um eco da mensagem e compara-la com o objeto mensagem que foi enviado
+    Resultado esperado: Uma mensagem identica a enviada.*/
+    public Teste1() {
+        Mensagem mensagem = new Mensagem();
+        mensagem.setDate(Date.valueOf("2015-02-12"));
+        mensagem.setTexto("Mensagem de teste");
+        mensagem.setNick("Tester");
+        MensagemDAO mensagemDAO = new MensagemDAO();
+        try {
+            mensagemDAO.Inserir(mensagem);
+        } catch (Exception ex) {
+            Logger.getLogger(Teste1.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showInternalMessageDialog(null,"Falha no envio de mensagem");
+        }
+        try {
+            List mensagens = mensagemDAO.ObterMensagens();
+            
+            
+        } catch (Exception ex) {
+            Logger.getLogger(Teste1.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Falha ao receber mensagem");
+        }
+    }    
+}
+/**try {
+            List mensagens = mensagemDao.SelectALL();
+            String textoMensagens = new String();
+            
+            Iterator i = mensagens.iterator();
+            while(i.hasNext()) {
+                Mensagem msg = (Mensagem) i.next();
+                textoMensagens += "\n" + msg.getNick() + ": " + msg.getMensagem();
+            }
+            jTxtAr_Historico.setText(textoMensagens);
+        }**/
--- Chat/src/chat/Teste2.java
+++ Chat/src/chat/Teste2.java
@@ -1,24 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 package chat;
 
 /**
  *
  * @author a1320548
  */
-public class Chat {
+public class Teste2 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-                
+    public Teste2() {
         /**Caso de teste 2**/
         /*Criar mais de um objeto do tipo mensagem, escrevê-la no banco de dados usando o método 
           MensagemDAO.InserirNovaMensagem(). Utilizar o método MensagemDAO.ObterMensagens
           para obter um eco das mensagens e compara-la com o objeto mensagem que foi enviado e se elas estão na ordem correta.
           Resultado esperado: Uma mensagem identica a enviada.*/
-        
-
-        
     }
     
 }
