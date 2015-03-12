3022478f8dd9b6b4e1e235c1ee5d58ea76287975
a1320726 <a1320726@L2-08.HTO.IFSP.EDU.BR>
2015-03-12 08:57:05 -0300

    Classe mensagem alteracda com novo usuario

--- Chat/src/chat/Mensagem.java
+++ Chat/src/chat/Mensagem.java
@@ -12,20 +12,19 @@ import java.util.Date;
  */
 public class Mensagem implements Comparable{
     
-    private String nick,texto;
+    private String texto;
     private Date date;
-    
-    
+    private Usuario usuario;
 
-
-    public String getNick() {
-        return nick;
+    public Usuario getUsuario() {
+        return usuario;
     }
 
-    public void setNick(String nick) {
-        this.nick = nick;
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
     }
-
+    
+    
     public String getTexto() {
         return texto;
     }
@@ -46,15 +45,14 @@ public class Mensagem implements Comparable{
         Mensagem msg2 = (Mensagem) t;
         
         if(this.date.compareTo(msg2.getDate())==0)
-            return this.nick.compareTo(msg2.getTexto());
+            return this.texto.compareTo(msg2.getTexto());
         else return this.date.compareTo(msg2.getDate());
     }
     @Override
     public boolean equals(Object t){
          Mensagem msg2 = (Mensagem) t;
          if(this.date.equals(msg2.getDate()) &&
-           this.texto.equals(msg2.getTexto()) &&
-           this.nick.equals(msg2.getNick()))
+           this.texto.equals(msg2.getTexto()) )
              return true;
         else return false;
     }
