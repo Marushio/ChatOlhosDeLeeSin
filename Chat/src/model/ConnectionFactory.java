ec85ca723cfc9a5bc1dcfed30800cec1c1bacef3
Marcio <Marcio@192.168.0.101>
2015-03-30 21:36:32 -0300

    Correção de bugs e erros conhecidos. Separação do programa em camadas: todos concluidos .

--- Chat/src/model/ConnectionFactory.java
+++ Chat/src/model/ConnectionFactory.java
@@ -38,12 +38,12 @@ public class ConnectionFactory {
         
         Connection connection = null;
         try {            
-            //String url="jdbc:mysql://192.168.1.28:3306/chat";
-            //String usuario="root";
+            String url="jdbc:mysql://192.168.1.28:3306/chat";
+            String usuario="root";
             
-            String url="jdbc:postgresql://localhost:5432/chat";
-            String usuario="postgres";
-            String senha="0";
+            //String url="jdbc:postgresql://localhost:5432/chat";
+            //String usuario="postgres";
+            String senha="";
             
             Class.forName("org.postgresql.Driver");
             connection = DriverManager.getConnection(url,usuario,senha);
